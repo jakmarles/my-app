f1bf4236b51913daa8f5dbab04e7231d99902add
ilya <jakmarles123@gmail.com>
2022-11-29 09:41:12 +0200

    55

--- src/Targil2.js
+++ src/Targil2.js
@@ -4,7 +4,7 @@ const Targil2 = () => {
   const [number1, setNumber1] = useState(0)
   const [number2, setNumber2] = useState(0)
   const [res, setRes] = useState(number1 + number2)
-  const Equalto = () =>{
+  const EqualTo = () =>{
       setRes (Number(number1)+Number(number2))
     }
   
@@ -14,7 +14,7 @@ const Targil2 = () => {
       <input placeholder='0' value={number1} onChange={e=>setNumber1(e.target.value)}></input>
         +
       <input placeholder='0' value={number2} onChange={e=>setNumber2(e.target.value)}></input><br/>
-      <button onClick={()=>Equalto()}>Res</button>
+      <button onClick={()=>EqualTo()}>Res</button>
       <h2>{res}</h2>
     </div>
   ) 
